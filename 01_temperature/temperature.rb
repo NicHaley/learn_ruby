6c76fa6a5f074015659f092273bd5bd7ed7adf59
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2014-11-02 20:03:24 -0500

    Finished 01_temperature

---
+++ 01_temperature/temperature.rb
@@ -0,0 +1,7 @@
+def ftoc(temp_f)
+	(temp_f - 32) * 5/9
+end
+
+def ctof(temp_c)
+	(temp_c * 9/5) + 32
+end
---
+++ temperature.rb
@@ -0,0 +1,7 @@
+def ftoc(f_temp)
+	0
+end
+
+# def ctof(c_temp)
+# 	c_temp * 9/5 + 32
+# end
